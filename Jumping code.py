7d4d3c28d6240aec7de9b320e28027a634f127c0
noname <butreimark@gmail.com>
2025-12-22 00:10:54 -0500

    -

--- Jumping code.py
+++ Jumping code.py
@@ -3,7 +3,7 @@ import arcade
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 500
 SCREEN_TITLE = ""
-GRAVITY=0.6
+GRAVITY=0.5
 JUMP=15
 class Dino(arcade.Sprite):
     in_jump_momentum=False
@@ -37,6 +37,15 @@ class Cactus(arcade.Sprite):
 class MyGame(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
+        """Game over """
+        self.game_status=True
+        self.game_over_picture=arcade.load_texture( "istockphoto-1325433246-640x640.jpg")
+
+
+
+
+
+
         """player point"""
         self.player_point=0
         """ background """
@@ -49,10 +58,27 @@ class MyGame(arcade.Window):
         self.Spiky_Cactus = Cactus("pixil-frame-0 (8).png", scale=3)
         self.Spiky_Cactus.center_x = SCREEN_WIDTH / 1.1
         self.Spiky_Cactus.center_y = SCREEN_HEIGHT / 1.8
+        """ game reset"""
+        self.game_reset()
+
+    def game_reset(self):
+         self.game_status = True
+         self.player_point = 0
+         self.dino_jumping.center_x = SCREEN_WIDTH / 2
+         self.dino_jumping.center_y = SCREEN_HEIGHT / 2
+         self.dino_jumping.change_y = 0
+         self.dino_jumping.in_jump_momentum = False
+
+         self.Spiky_Cactus.center_x = SCREEN_WIDTH / 1.1
+         self.Spiky_Cactus.center_y = SCREEN_HEIGHT / 1.8
+         self.Spiky_Cactus.change_x = -4
+         self.Spiky_Cactus.point_counter = True
     def on_key_press(self, symbol: int, modifiers: int):
         if symbol == arcade.key.SPACE and not self.dino_jumping.in_jump_momentum:
             self.dino_jumping.change_y=JUMP
             self.dino_jumping.in_jump_momentum=True
+        if symbol == arcade.key.R:
+            self.game_reset()
 
 
     def on_key_release(self, symbol: int, modifiers: int):
@@ -63,12 +89,18 @@ class MyGame(arcade.Window):
         arcade.draw_texture_rectangle(SCREEN_WIDTH/2,SCREEN_HEIGHT/2,SCREEN_WIDTH,SCREEN_HEIGHT,self.background_picture)
         self.dino_jumping.draw()
         self.Spiky_Cactus.draw()
-        arcade.draw_text(f"P{self.player_point}",200,420,arcade.color.ALABAMA_CRIMSON,50)
-
+        arcade.draw_text(f"Points:{self.player_point}",20,480,arcade.color.ORANGE,20)
+        #self.dino_jumping.draw_hit_box((255, 0, 0), 3)
+        #self.Spiky_Cactus.draw_hit_box((255, 0, 0), 3)
+        if self.game_status==False:
+            arcade.draw_texture_rectangle(SCREEN_WIDTH/2,SCREEN_HEIGHT/2,SCREEN_WIDTH,SCREEN_HEIGHT,self.game_over_picture)
     def on_update(self, delta_time):
-        self.dino_jumping.movement()
-        self.Spiky_Cactus.movement()
+        if  self.game_status:
 
+            self.dino_jumping.movement()
+            self.Spiky_Cactus.movement()
+        if arcade.check_for_collision(self.dino_jumping,self.Spiky_Cactus):
+            self.game_status=False
 window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 arcade.run()
 
